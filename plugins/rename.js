3106f6829e25ddf9e8ca1c48a0f6b679dfbcf6cc
Til Blechschmidt <til@blechschmidt.de>
2015-07-22 23:23:07 +0200

    Moved rename to rename.js

--- plugins/rename.js
+++ plugins/rename.js
@@ -1,7 +1,15 @@
+var log = require('lib/log')
+    , helpers = require('lib/helpers');
+
 module.exports = {
     channels: {
-        rename: function (message, log) {
-            log.mesh.info("rename msg");
+        rename: function (message) {
+            log.mesh.info("rename received");
+            if (message.data.name) settings.hostname = message.data.name;
+            helpers.writeSettings(settings);
         }
+    },
+    rename: function (currentID, newID) {
+        anyMesh.request(currentID, {"channel": "rename", "name": newID});
     }
 };
