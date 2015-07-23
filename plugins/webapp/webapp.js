f79043e086772655853632d44b8fc26fc40a0949
Til Blechschmidt <til@blechschmidt.de>
2015-07-23 19:43:39 +0200

    Added binary check for meteorJS

--- plugins/webapp/webapp.js
+++ plugins/webapp/webapp.js
@@ -1,6 +1,7 @@
 var log = require('lib/log')()
     , net = require('net')
     , path = require('path')
+    , hasbin = require('hasbin')
     , spawn = require('child_process').spawn
     , meteor = null;
 
@@ -14,18 +15,27 @@ module.exports = {
             log.plugin.trace("WEB received message");
         }
     },
-    load: function () {
-        var server = net.createServer(function (c) { //'connection' listener
-            //console.log('client connected');
-            c.on('end', function () {
-                //console.log('client disconnected');
-            });
-            c.write('hello\r\n');
-            c.pipe(c);
-        });
-        server.listen(8124, function () { //'listening' listener
-            log.plugin.debug('WEB TCP Server online');
-            spawn('meteor', [], {cwd: path.join(__dirname, 'meteor')});
+    load: function (cb) {
+        //Check for dependencies
+        hasbin('meteor', function (installed) {
+            if (!installed) {
+                if (typeof cb === 'function') cb(false);
+            } else {
+                cb(true);
+                var server = net.createServer(function (c) { //'connection' listener
+                    //console.log('client connected');
+                    c.on('end', function () {
+                        //console.log('client disconnected');
+                    });
+                    c.write('hello\r\n');
+                    c.pipe(c);
+                });
+                server.listen(8124, function () { //'listening' listener
+                    log.plugin.debug('WEB TCP Server online');
+                    //ToDo: Add check if meteor binary exists
+                    spawn('meteor', [], {cwd: path.join(__dirname, 'meteor')});
+                });
+            }
         });
     }
 };
