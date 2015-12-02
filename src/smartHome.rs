a19ed0a68579a5a780df2c44f504fcb061208162
Til Blechschmidt <til@blechschmidt.de>
2015-12-02 20:13:59 +0100

    Created library out of networking.rs

---
+++ src/smartHome.rs
@@ -0,0 +1,18 @@
+mod networking;
+use networking::*;
+
+fn main() {
+    let sock = Connection::new().build();
+    let rx = sock.recv_thread();
+
+    println!("LISN {}", sock.socket.local_addr().unwrap());
+
+    for data in rx.iter() {     // Infinite iterator over incoming messages
+        let (mut buf, src) = data;
+        println!("RECV {}/{}/{} -> {}   @   {}", buf[0], buf[1], buf[2], buf[3], src);
+        let buf = &mut buf[..4];
+        buf.reverse();
+        println!("SEND {}/{}/{} -> {}   @   {}", buf[0], buf[1], buf[2], buf[3], src);
+        sock.send(&buf, src);
+    }
+}
