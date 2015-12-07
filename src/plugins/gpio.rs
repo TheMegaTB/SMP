b2ae4eefd979b20ea1d0b10f886821c91e723815
Til Blechschmidt <til@blechschmidt.de>
2015-12-07 17:24:47 +0000

    Fixed command invocation

--- src/plugins/gpio.rs
+++ src/plugins/gpio.rs
@@ -17,7 +17,11 @@ impl GPIO {
 }
 
 pub fn new_listener() -> mpsc::Sender<([u8; 4], SocketAddr)> {
-    Command::new("/usr/local/bin/gpio -g mode 24 out && /usr/local/bin/gpio -g write 24 1").status().unwrap_or_else(|e| {
+    Command::new("/usr/local/bin/gpio").arg("-g").arg("mode").arg("24").arg("out").status().unwrap_or_else(|e| {
+        panic!("failed to execute process: {}", e)
+    });
+
+    Command::new("/usr/local/bin/gpio").arg("-g").arg("write").arg("24").arg("1").status().unwrap_or_else(|e| {
         panic!("failed to execute process: {}", e)
     });
 
@@ -25,11 +29,10 @@ pub fn new_listener() -> mpsc::Sender<([u8; 4], SocketAddr)> {
     thread::Builder::new().name("plugin_gpio".to_string()).spawn(move || {
         let gpio = GPIO::listen(rx);
         for data in gpio.rx.iter() {
-            println!("GPIO RECEIVED SOME DATA ({})", data.0[0]);
             let id = data.0;
             if id[0] == 0 && id[1] == 0 && id[2] == 0 {
-                println!("ID MATCHES! NOW DOING SMTH");
-                Command::new(format!("/usr/local/bin/gpio -g mode 24 {}", !id[3])).status().unwrap_or_else(|e| {
+		let value = (id[3]).to_string();
+                Command::new("/usr/local/bin/gpio").arg("-g").arg("write").arg("24").arg(value).status().unwrap_or_else(|e| {
                     panic!("failed to execute process: {}", e)
                 });
             }
