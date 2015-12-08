ce2f0dcbb7be63702ad3a696e28b6ef10c45cdb3
Til Blechschmidt <til@blechschmidt.de>
2015-12-08 20:45:13 +0100

    Fixed RECV formatting

--- examples/cmd-client.rs
+++ examples/cmd-client.rs
@@ -20,8 +20,8 @@ fn main() {     // TODO: Convert this to a VALID working implementation test.
     println!("SEND {}/{}/{} -> {}   @   233.233.233.233:8000", data[0], data[1], data[2], data[3]);
     tx.send((data, socket_addr_from_str("233.233.233.233:8000"))).unwrap();
 
-    for d in rx.iter() { //TODO: Resend messages if the correct echo is not being received
-        println!("RECV {}/{}/{} -> {}   @   {}", d.0[0], d.0[1], d.0[2], d.0[3], d.1);
+    for d in rx.iter() { //TODO: Resend datagram if the correct echo is not being received after a certain timeout
+        println!("RECV {} <- {}\\{}\\{}   @   {}", d.0[0], d.0[1], d.0[2], d.0[3], d.1);
         let mut x = d.0;
         x.reverse();
         if x == data {
