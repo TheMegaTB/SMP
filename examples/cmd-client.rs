794ec1d3835e2c5f785f4970f97e07e16e580673
Til Blechschmidt <til@blechschmidt.de>
2016-01-04 15:20:16 +0100

    Added non-blocking timeout

--- examples/cmd-client.rs
+++ examples/cmd-client.rs
@@ -2,6 +2,7 @@ extern crate networking;
 extern crate rand;
 use networking::*;
 use std::env;
+use std::thread;
 use std::str::FromStr;
 
 fn main() {     // TODO: Convert this to a VALID working implementation test.
@@ -20,13 +21,25 @@ fn main() {     // TODO: Convert this to a VALID working implementation test.
     println!("SEND {}/{}/{} -> {}   @   233.233.233.233:8000", data[0], data[1], data[2], data[3]);
     tx.send((data, socket_addr_from_str("233.233.233.233:8000"))).unwrap();
 
-    for d in rx.iter() { //TODO: Resend datagram if the correct echo is not being received after a certain timeout
-        println!("RECV {} <- {}\\{}\\{}   @   {}", d.0[0], d.0[1], d.0[2], d.0[3], d.1);
-        let mut x = d.0;
-        x.reverse();
-        if x == data {
-            println!("RECEIVED REVERSE ECHO!");
-            break;
+    let sleep_duration = 100;
+    let mut timeout = 5000;
+    loop {
+        match rx.try_recv() {
+            Result::Ok(d) => {
+                println!("RECV {} <- {}\\{}\\{}   @   {}", d.0[0], d.0[1], d.0[2], d.0[3], d.1);
+                let mut x = d.0;
+                x.reverse();
+                if x == data {
+                    println!("RECEIVED REVERSE ECHO!");
+                    break;
+                }
+            },
+            Result::Err(err) => {
+                if err == std::sync::mpsc::TryRecvError::Disconnected { panic!("Channel disconnected!") }
+                thread::sleep_ms(sleep_duration);
+                timeout = timeout - sleep_duration;
+                if timeout <= 0 { panic!("Timeout.") }
+            }
         }
     }
 }
