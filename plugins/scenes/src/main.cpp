460bf08163bac2461c22d944e74eac2f7177face
Til Blechschmidt <til@blechschmidt.de>
2017-02-05 18:32:22 +0100

    Added scenes plugin functionality

--- plugins/scenes/src/main.cpp
+++ plugins/scenes/src/main.cpp
@@ -4,7 +4,29 @@
 using json = nlohmann::json;
 
 void callback(Plugin *context, string action, Channel *c, json p) {
+    json mappings = context->config["mappings"];
+    if (c != nullptr && mappings[c->getAddressAsString()].is_object()) {
+        int value = p["payload"];
 
+        json sequence;
+        if ((bool) value) {
+            sequence = mappings[c->getAddressAsString()]["on"];
+        } else {
+            sequence = mappings[c->getAddressAsString()]["off"];
+        }
+
+        for (json::iterator it = sequence.begin(); it != sequence.end(); ++it) {
+            json payload = (*it)[3];
+            (*it).erase((*it).begin() + 3);
+
+            json dev = {
+                    {"action",  "write"},
+                    {"channel", (*it)},
+                    {"payload", payload}
+            };
+            context->outgoingDatagrams.add(dev.dump());
+        }
+    }
 }
 
 int init(Plugin *context) {
