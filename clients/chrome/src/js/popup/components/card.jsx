87072f9c041d6a6301e9bc70915fbddb47991be5
Til Blechschmidt <til@blechschmidt.de>
2017-02-05 22:43:34 +0100

    Restyled cards

--- clients/chrome/src/js/popup/components/card.jsx
+++ clients/chrome/src/js/popup/components/card.jsx
@@ -4,10 +4,10 @@ export default class Card extends React.Component {
     render() {
         return (
             <div className="mdl-shadow--4dp mdl-card" style={{width: "85%", margin: "20px auto"}}>
-                <div className="mdl-card__title" style={{padding: "16px 16px 0 16px"}}>
+                <div className="mdl-card__title">
                     <h2 className="mdl-card__title-text">{this.props.title}</h2>
                 </div>
-                <div className="mdl-card__supporting-text">
+                <div className="mdl-card__actions mdl-card--border">
                     {this.props.content}
                 </div>
             </div>
