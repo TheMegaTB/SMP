8e15247fc6bdc599d7aa94d0be70c613cda549b4
Til Blechschmidt <til@blechschmidt.de>
2017-02-05 18:39:30 +0100

    Simplified scenes config

--- plugins/scenes/src/main.cpp
+++ plugins/scenes/src/main.cpp
@@ -4,15 +4,14 @@
 using json = nlohmann::json;
 
 void callback(Plugin *context, string action, Channel *c, json p) {
-    json mappings = context->config["mappings"];
-    if (c != nullptr && mappings[c->getAddressAsString()].is_object()) {
+    if (c != nullptr && context->config[c->getAddressAsString()].is_object()) {
         int value = p["payload"];
 
         json sequence;
         if ((bool) value) {
-            sequence = mappings[c->getAddressAsString()]["on"];
+            sequence = context->config[c->getAddressAsString()]["on"];
         } else {
-            sequence = mappings[c->getAddressAsString()]["off"];
+            sequence = context->config[c->getAddressAsString()]["off"];
         }
 
         for (json::iterator it = sequence.begin(); it != sequence.end(); ++it) {
